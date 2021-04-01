6cccb33c1ed0e1fa19892955e1b1655743c24e55
Stewar <stewar1020@gmail.com>
2021-04-01 15:53:51 -0500

    Solving Ejercicio2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
+    int number1, number2;
+    Scanner scn = new Scanner(System.in);
+
     public static void main(String[] args) {
-        int number1 = 1, number2 = 10;
         Main main = new Main();
-        main.bigger(number1,number2);
+        main.readNumbers();
+        main.bigger();
     }
 
-    public void bigger(int number1, int number2) {
+    public void readNumbers() {
+        System.out.println("Ingrese el primer numero: ");
+        number1 = scn.nextInt();
+        System.out.println("Ingrese el segundo numero: ");
+        number2 = scn.nextInt();
+    }
+
+
+    public void bigger() {
         if (number1 > number2) {
             System.out.println("El numero " + number1 + " es mayor que " + number2);
         } else if (number2 > number1) {
